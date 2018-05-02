a7db28970db35d405287e8d9f1cfa4edf215dfa2
Max <pingwinno@gmail.com>
2018-05-02 17:27:57 +0300

    Add additional checks and log messages. Do some refacrtoring.

--- src/main/java/com/pingwinno/notification/handler/CommandLineRunner.java
+++ src/main/java/com/pingwinno/notification/handler/CommandLineRunner.java
@@ -1,32 +1,30 @@
 package com.pingwinno.notification.handler;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class CliInterface {
+public class CommandLineRunner {
 
 
-    public void executeCommand(String name, String time) {
+    public void executeCommand(String name, String time) throws InterruptedException, IOException {
         //command line for run streamlink
         String fileName = name +"_"+ time + ".mp4";
         String command = String.join(" ","streamlink","https://www.twitch.tv/olyashaa", "best",  "-o", fileName);
         StringBuffer output = new StringBuffer();
 
         Process p;
-        try {
+            System.out.println("Streamlink start");
             p = Runtime.getRuntime().exec(command);
             p.waitFor();
-            BufferedReader reader =
-                    new BufferedReader(new InputStreamReader(p.getInputStream()));
+            try (BufferedReader reader =
+                    new BufferedReader(new InputStreamReader(p.getInputStream()));){
 
             String line = "";
             while ((line = reader.readLine())!= null) {
                 output.append(line + "\n");
             }
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+            }
 
         System.out.println(output.toString());
 
