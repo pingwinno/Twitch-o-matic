42dccf7f762b853c543fb8c618437e55cb947848
Max <pingwinno@gmail.com>
2018-05-10 04:31:41 +0300

    Revert "Add additional checks and log messages."
    This reverts commit 28ca613c0fa1edf0cd3c7d7e1ea4f9019037aae6.
    
    # Conflicts:
    #	src/main/java/com/pingwinno/controllers/HttpRequestHandler.java

--- src/main/java/com/pingwinno/controllers/HttpRequestHandler.java
+++ src/main/java/com/pingwinno/controllers/HttpRequestHandler.java
@@ -1,6 +1,7 @@
 package com.pingwinno.controllers;
 
 
+import com.pingwinno.notification.handler.CliInterface;
 import com.pingwinno.notification.handler.CommandLineRunner;
 import com.pingwinno.notification.handler.DataModel;
 import com.pingwinno.notification.handler.NotificationModel;
@@ -25,16 +26,18 @@ public class HttpRequestHandler {
 
     @GET
     public Response getQuery(@Context UriInfo info) {
+
         Response response = null;
         if (info != null) {
+
             String hubMode = info.getQueryParameters().getFirst("hub.mode");
             //handle denied response
             if (hubMode.equals("denied")) {
                 String hubReason = info.getQueryParameters().getFirst("hub.reason");
-               response = Response.status(Response.Status.OK).build();
+                response = Response.status(Response.Status.OK).build();
                 System.out.println(hubMode + " " + hubReason);
                 System.out.println("denied");
-
+                return response;
             }
 //handle verify response
             else {
@@ -42,13 +45,13 @@ public class HttpRequestHandler {
                 response = Response.status(Response.Status.OK).entity(hubChallenge).build();
                 System.out.println(hubMode + " " + hubChallenge);
                 System.out.println("accepted");
-            }
 
+            }
         }
         else System.out.println("Response is not correct");
-        return response;
-    }
+            return response;
 
+    }
 
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
@@ -59,7 +62,7 @@ public class HttpRequestHandler {
             NotificationModel notificationModel = notificationArray[0];
             //check for notification duplicate
             if (!(notificationModel.getId().equals(lastNotificationId))) {
-                lastNotificationId = notificationModel.getId();
+
                 CommandLineRunner commandLineRunner = new CommandLineRunner();
 
                 new Thread(() -> commandLineRunner.executeCommand(notificationModel.getTitle(), notificationModel.getStarted_at())).start();
