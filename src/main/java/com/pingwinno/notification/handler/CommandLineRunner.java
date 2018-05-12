8e458cffea21e795d1bb9a06455ae81ad47a9ccc
Max <pingwinno@gmail.com>
2018-05-12 08:13:07 +0300

    Close BufferedReader

--- src/main/java/com/pingwinno/notification/handler/CommandLineRunner.java
+++ src/main/java/com/pingwinno/notification/handler/CommandLineRunner.java
@@ -5,33 +5,25 @@ import java.io.InputStreamReader;
 
 public class CommandLineRunner {
 
-
     public void executeCommand(String time, String user) {
         //command line for run streamlink
         String fileName = time + ".mp4";
-        String command = String.join(" ","streamlink","https://www.twitch.tv/" + user, "best",  "-o", fileName);
-        StringBuffer output = new StringBuffer();
-
+        String command = String.join(" ", "streamlink", "https://www.twitch.tv/" + user, "best", "-o", fileName);
+        StringBuilder output = new StringBuilder();
         Process p;
         try {
             p = Runtime.getRuntime().exec(command);
             p.waitFor();
             BufferedReader reader =
                     new BufferedReader(new InputStreamReader(p.getInputStream()));
-
             String line = "";
-            while ((line = reader.readLine())!= null) {
+            while ((line = reader.readLine()) != null) {
                 output.append(line + "\n");
             }
-
-
+            reader.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
-
-
         System.out.println(output.toString());
-
     }
-
 }
