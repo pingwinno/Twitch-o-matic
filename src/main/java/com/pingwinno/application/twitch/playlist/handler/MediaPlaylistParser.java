7d5ba8cb489d8a1081b610df0d4b9b181fcb1450
shiro <pingwinno@gmail.com>
2018-07-08 19:08:52 -0700

    fix parsing muted chunks

--- src/main/java/com/pingwinno/application/twitch/playlist/handler/MediaPlaylistParser.java
+++ src/main/java/com/pingwinno/application/twitch/playlist/handler/MediaPlaylistParser.java
@@ -5,12 +5,15 @@ import java.io.IOException;
 import java.util.LinkedHashSet;
 
 public class MediaPlaylistParser {
-    public static LinkedHashSet<String> parse (BufferedReader reader) throws IOException {
+    public static LinkedHashSet<String> parse(BufferedReader reader) throws IOException {
         String string;
         LinkedHashSet<String> chunks = new LinkedHashSet<>();
         while ((string = reader.readLine()) != null) {
             if (string.contains("#EXTINF")) {
-                chunks.add(reader.readLine());
+                string = reader.readLine();
+                if (!string.contains("muted")) {
+                    chunks.add(reader.readLine());
+                }
             }
         }
         return chunks;
