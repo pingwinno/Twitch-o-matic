8b2acebd3e994a36d7941a9bc018e0e290e52135
pingwinno <pingwinno@gmail.com>
2018-10-14 05:23:11 +0300

    Update easy_install.sh

--- easy_install.sh
+++ easy_install.sh
@@ -6,7 +6,7 @@ echo -e "\033[36m Installing dependencies.\033[0m"
 apt install -y maven git
 ### installing Twitch-o-matic
 git clone https://github.com/pingwinno/Twitch-o-matic.git
-cd ./twitch-o-matic
+cd ./Twitch-o-matic
 mvn package
 mkdir /usr/local/Twitch-o-matic/
 mkdir  $HOME/.config/
@@ -18,7 +18,7 @@ chmod +x /usr/local/bin/Twitch-o-matic.sh
 
 cp Twitch-o-matic.service /lib/systemd/system/
 cd ../../
-rm -rf twitch-o-matic
+rm -rf Twitch-o-matic
 chmod 644 /lib/systemd/system/Twitch-o-matic.service
 systemctl daemon-reload
 systemctl enable Twitch-o-matic.service
