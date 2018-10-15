eb5900a03eeedc61f8da0cdd91f4364137dc0352
shiro <pingwinn@gmail.com>
2018-10-15 14:48:07 -0700

    Add downloaded stream validation

--- src/main/java/com/pingwinno/presentation/management/api/ManagementApiHandler.java
+++ src/main/java/com/pingwinno/presentation/management/api/ManagementApiHandler.java
@@ -14,12 +14,13 @@ import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.io.IOException;
+import java.util.UUID;
 
-@Path("/start")
+
 public class ManagementApiHandler {
     private org.slf4j.Logger log = LoggerFactory.getLogger(getClass().getName());
 
-
+    @Path("/start")
     @GET
     @Produces(MediaType.TEXT_HTML)
     public Response startRecord(@QueryParam("type") String type, @QueryParam("value") String value) {
@@ -46,6 +47,43 @@ public class ManagementApiHandler {
                     response = Response.accepted().build();
                 } else {
                     log.error("Stream {] not found", value);
+                    response = Response.status(Response.Status.NOT_FOUND).build();
+                }
+            } else {
+                response = Response.status(Response.Status.NOT_ACCEPTABLE).build();
+            }
+        } catch (IOException | InterruptedException e) {
+            response = Response.status(500, e.toString()).build();
+            log.error("Can't start record {}", e);
+        }
+        return response;
+    }
+
+    @Path("/start")
+    @GET
+    @Produces(MediaType.TEXT_HTML)
+    public Response validateRecord(@QueryParam("vodId") String vodId, @QueryParam("uuid") String uuid) {
+        Response response;
+        StreamExtendedDataModel streamMetadata = null;
+        try {
+            log.trace("vodId: {}",vodId);
+            log.trace("uuid: {}",uuid);
+
+                streamMetadata = VodMetadataHelper.getVodMetadata(vodId);
+
+            if (streamMetadata != null) {
+                VodDownloader vodDownloader = new VodDownloader();
+                if (streamMetadata.getVodId() != null) {
+
+                    streamMetadata.setUuid(UUID.fromString(uuid));
+                    StreamExtendedDataModel finalStreamMetadata = streamMetadata;
+                    new Thread(() -> vodDownloader.initializeDownload(finalStreamMetadata)).start();
+
+                    String startedAt = streamMetadata.getDate();
+                    log.info("Record started at:{} ", startedAt);
+                    response = Response.accepted().build();
+                } else {
+                    log.error("Stream {] not found", vodId);
                     response = Response.status(Response.Status.NOT_FOUND).build();
                 }
             } else {
