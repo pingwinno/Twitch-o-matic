8f16415e89c50c20177a27bd8f4af2bccfae6790
pingwinno <pingwinno@gmail.com>
2018-12-25 09:40:19 +0200

    Update easy_install.sh

--- easy_install.sh
+++ easy_install.sh
@@ -4,7 +4,7 @@ cd /tmp
 
 ### installing Twitch-o-matic
 
-FILE=/usr/local/twitch-o-matic/Twitch-o-matic.jar
+FILE=/usr/local/twitch-o-matic/twitch-o-matic.jar
 if [ -f $FILE ]; then
   echo "tom installed. Updating...."
   git clone https://github.com/pingwinno/Twitch-o-matic.git
@@ -16,7 +16,7 @@ else
   apt update
   apt upgrade -y
   apt install -y maven git jsvc
-  git clone https://github.com/pingwinno/Twitch-o-matic.git
+  git clone https://github.com/pingwinno/twitch-o-matic.git
   cd ./Twitch-o-matic
   mvn package
   adduser --system --no-create-home --group tom-daemon
@@ -32,14 +32,14 @@ else
 fi
 
 
-mv ./target/Twitch-o-matic.jar /usr/local/twitch-o-matic/
+mv ./target/twitch-o-matic.jar /usr/local/twitch-o-matic/
 mv twitch-o-matic.sh /usr/local/bin/
 chmod +x /usr/local/bin/twitch-o-matic.sh
 
 
 cp twitch-o-matic.service /etc/systemd/system/twitch-o-matic.service
 cd ../
-rm -rf Twitch-o-matic
+rm -rf twitch-o-matic
 chmod 644 /etc/systemd/system/twitch-o-matic.service
 systemctl daemon-reload
 systemctl enable twitch-o-matic.service
