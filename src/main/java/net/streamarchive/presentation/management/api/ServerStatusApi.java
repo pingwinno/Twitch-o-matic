d5f8058c77b91edadf2d482a49f723baf7af30a3
pingwinno <pingwinno>
2019-10-14 12:02:05 +0300

    Updated databases sync

--- src/main/java/net/streamarchive/presentation/management/api/ServerStatusApi.java
+++ src/main/java/net/streamarchive/presentation/management/api/ServerStatusApi.java
@@ -1,39 +1,38 @@
 package net.streamarchive.presentation.management.api;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import net.streamarchive.application.StorageHelper;
 import net.streamarchive.infrastructure.SettingsProperties;
+import net.streamarchive.infrastructure.handlers.db.JpaDBHandler;
+import net.streamarchive.infrastructure.handlers.db.RestDBHandler;
 import net.streamarchive.infrastructure.models.StorageState;
 import net.streamarchive.infrastructure.models.Streamer;
+import net.streamarchive.infrastructure.models.StreamerNotFoundException;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 /**
  * API returns storage state.
  */
-@PreAuthorize("#username == authentication.principal.username")
+
 @RestController
 @RequestMapping("/api/v1/server")
 public class ServerStatusApi {
-
     private final
     SettingsProperties settingsProperties;
     private final
     StorageHelper storageHelper;
+    @Autowired
+    JpaDBHandler jpaDBHandler;
+    @Autowired
+    RestDBHandler restDBHandler;
     private org.slf4j.Logger log = LoggerFactory.getLogger(getClass().getName());
 
     @Autowired
@@ -58,48 +57,31 @@ public class ServerStatusApi {
      * Method does import to local json files from MongoDB
      */
     @GetMapping("/import")
-    public void importToLocalDb() {
-        //TODO update db method
-            /*
-            for (User user : settingsProperties.getUsers()) {
-                for (StreamDocumentModel stream : mongoTemplate.findAll(StreamDocumentModel.class, user.getUser())) {
-
-                    dataBaseWriter.writeToRemoteDB(stream, user.getUser(), false);
-
-
-                }
+    public void importToLocalDb() throws StreamerNotFoundException {
+        for (Streamer streamer : settingsProperties.getUsers()) {
+            for (net.streamarchive.infrastructure.models.Stream stream : restDBHandler.getAllStreams(streamer.getName())) {
+                stream.setStreamer(streamer.getName());
+                log.trace("Saving " + stream.toString());
+                jpaDBHandler.addStream(stream);
+                log.trace(stream.toString() + " saved");
             }
- */
+        }
     }
 
     /**
      * Method does export from local db to MongoDB
-     *
      */
     @GetMapping("/export")
-    public void exportFromLocalDb() {
-        ObjectMapper objectMapper = new ObjectMapper();
-
+    public void exportFromLocalDb() throws StreamerNotFoundException {
         for (Streamer streamer : settingsProperties.getUsers()) {
-
-            try (Stream<java.nio.file.Path> walk = Files.walk(Paths.get(settingsProperties.getRecordedStreamPath() + streamer))) {
-
-                List<String> result = walk.filter(Files::isDirectory)
-                        .map(Path::toString).collect(Collectors.toList());
-
-                result.forEach(x -> {
-                    //TODO update db method
-            /*
-                        dataBaseWriter.writeToRemoteDB(objectMapper.readValue(x, StreamDocumentModel.class), user.getUser(), true);
-
-
-             */
-                });
-
-            } catch (IOException e) {
-                throw new InternalServerErrorExeption();
+            for (net.streamarchive.infrastructure.models.Stream stream : jpaDBHandler.getAllStreams(streamer.getName())) {
+                stream.setStreamer(streamer.getName());
+                log.trace("Saving " + stream.toString());
+                restDBHandler.addStream(stream);
+                log.trace(stream.toString() + " saved");
             }
         }
+
     }
 
 
